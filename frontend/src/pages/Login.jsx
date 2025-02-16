8f8fe25c94f8ea28365e73e13d87ed3d58bcbad0
naolumer <naolumer3@gmail.com>
2025-02-16 17:01:42 +0300

    UI for signup/login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state,setState] = useState("Sign Up")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [name,setName] = useState("")
+
+  const onSubmitHandler = async(event)=>{
+    event.preventDefault()
+  }
+  const onclickHandler = ()=>{
+    state === "Sign Up"?setState("Login"):setState("Sign Up")
+  }
+
   return (
-    <div>Login</div>
+    <div className='flex flex-col gap-3 items-start justify-center mt-24 mb-[250px] mx-auto border border-gray-200 rounded-xl shadow-md px-8 py-8 w-[350px] sm:w-[380px] max-h-[600px]'>
+      <p className='text-2xl text-gray-600 font-medium'>{state==="Sign Up"?"Create Account":"Login"}</p>
+      <p className=' text-gray-600  text-sm'>Please <span>{state==="Sign Up"?"sign up ":"log in"}</span> to book appointment</p>
+      {/* <p className='text-sm text-gray-500'>Please log in to book appointment</p> */}
+      <form onSubmit={onSubmitHandler} className='flex flex-col items-center gap-3 ' >
+        {
+          state==="Login"?"":
+          <label className='flex flex-col gap-1 text-sm  text-gray-600'>
+          Full Name
+          <input  className='w-full px-12 sm:px-16 py-[8px] rounded border border-gray-300 ' onChange={(e)=>setName(e.target.value)} value={name} type="text" />
+        </label>
+        }
+        <label className='flex flex-col gap-1 text-sm  text-gray-600'>
+          Email
+          <input className='px-12 sm:px-16 py-[8px] rounded border border-gray-300 text-start' onChange={(e)=>setEmail(e.target.value)} value={email} type="email" />
+        </label>
+
+        <label className='flex flex-col gap-1 text-sm text-gray-600 mb-3'>
+          Password
+          <input className='px-12 sm:px-16 py-[8px] rounded border border-gray-300 ' onChange={(e)=>setPassword(e.target.value)} value={password}  type="password" />
+        </label>
+
+        <button className='bg-primary text-white w-full py-[8px] rounded-md cursor-pointer' type='submit'>{state}</button>
+      </form>
+
+      <p className='text-gray-600 text-sm pt-2'>{state==="Login"?"Create a new account?":"Already have an account?"}<span onClick={onclickHandler} className='text-sm text-primary underline cursor-pointer'>{state==="Sign Up"?" Login":" Click "} here</span></p>
+    </div>
   )
 }
 
