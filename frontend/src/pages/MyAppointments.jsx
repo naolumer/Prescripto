483a9531ea3a167fda5d4e71ac7609e8942739ea
naolumer <naolumer3@gmail.com>
2025-02-18 19:15:01 +0300

    fixed some bugs

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -9,7 +9,7 @@ const MyAppointments = () => {
       <div>
         {
           doctors.slice(0,3).map((item,index)=>(
-          <div className='grid frid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
+          <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
             <div>
               <img className='w-32 bg-indigo-50' src={item.image} alt="" />
             </div>
