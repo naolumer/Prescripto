1957d52d63be4829dd71a267c77204a42e46b6c2
naolumer <naolumer3@gmail.com>
2025-02-22 11:51:04 +0300

    created add doctor functionality from admin panel

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AdminContextProvider>
     <DoctorContextProvider>
-      ,<AppContextProvider>
+      <AppContextProvider>
         <App />
       </AppContextProvider>
     </DoctorContextProvider>
