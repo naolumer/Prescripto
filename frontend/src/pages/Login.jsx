bff1ed0405bc8049905dfc32f9d702a0d7060663
naolumer <naolumer3@gmail.com>
2025-03-25 09:22:33 +0300

    improved login ui for the frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,23 +19,31 @@ const Login = () => {
       <p className='text-2xl text-gray-600 font-medium'>{state==="Sign Up"?"Create Account":"Login"}</p>
       <p className=' text-gray-600  text-sm'>Please <span>{state==="Sign Up"?"sign up ":"log in"}</span> to book appointment</p>
       {/* <p className='text-sm text-gray-500'>Please log in to book appointment</p> */}
-      <form onSubmit={onSubmitHandler} className='flex flex-col items-center gap-3 ' >
+      <form onSubmit={onSubmitHandler} className='flex flex-col items-center gap-3 w-full ' >
         {
           state==="Login"?"":
+        <div className='w-full'>
           <label className='flex flex-col gap-1 text-sm  text-gray-600'>
           Full Name
-          <input  className='w-full px-12 sm:px-16 py-[8px] rounded border border-gray-300 ' onChange={(e)=>setName(e.target.value)} value={name} type="text" />
+          <input  className='px-4 w-full  py-[8px] rounded border border-gray-300 ' onChange={(e)=>setName(e.target.value)} value={name} type="text" />
         </label>
+        </div>
+          
         }
-        <label className='flex flex-col gap-1 text-sm  text-gray-600'>
+        <div className='w-full'>
+          <label className='flex flex-col gap-1 text-sm  text-gray-600'>
           Email
-          <input className='px-12 sm:px-16 py-[8px] rounded border border-gray-300 text-start' onChange={(e)=>setEmail(e.target.value)} value={email} type="email" />
+          <input className='px-4 w-full  py-[8px] rounded border border-gray-300 text-start' onChange={(e)=>setEmail(e.target.value)} value={email} type="email" />
         </label>
-
-        <label className='flex flex-col gap-1 text-sm text-gray-600 mb-3'>
+        </div>
+        
+        <div className='w-full'>
+          <label className='flex flex-col gap-1 text-sm text-gray-600 mb-3'>
           Password
-          <input className='px-12 sm:px-16 py-[8px] rounded border border-gray-300 ' onChange={(e)=>setPassword(e.target.value)} value={password}  type="password" />
+          <input className='px-4 w-full  py-[8px] rounded border border-gray-300 ' onChange={(e)=>setPassword(e.target.value)} value={password}  type="password" />
         </label>
+        </div>
+        
 
         <button className='bg-primary text-white w-full py-[8px] rounded-md cursor-pointer' type='submit'>{state}</button>
       </form>
