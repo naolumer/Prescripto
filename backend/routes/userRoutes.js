a02cf24b3fdef4b80ca8f32cba79551143a41aa1
naolumer <naolumer3@gmail.com>
2025-03-28 10:13:38 +0300

    display user appointments in my-appointments page from the backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { bookAppointment, getProfile, login, register, updateProfile } from "../controllers/userControllers.js"
+import { bookAppointment, getProfile, listAppointment, login, register, updateProfile } from "../controllers/userControllers.js"
 import authUser from "../middlewares/authUser.js"
 import upload from "../middlewares/multer.js"
 
@@ -10,5 +10,6 @@ userRouter.post("/login", login)
 userRouter.get("/get-profile",authUser,getProfile)
 userRouter.post("/update-profile",upload.single("image"),authUser,updateProfile)
 userRouter.post("/book-appointment",authUser,bookAppointment)
+userRouter.get("/appointments",authUser,listAppointment)
 
 export default userRouter
