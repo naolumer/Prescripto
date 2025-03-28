29ad5f3a3acc1f89c1947a96daabe1cad5b186b3
naolumer <naolumer3@gmail.com>
2025-03-28 09:50:18 +0300

    removing booked time slots

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -56,11 +56,23 @@ const Appointment = () => {
       while (currentDate < endTime){
         let formattedTime = currentDate.toLocaleTimeString([],{hour:"2-digit",minute:"2-digit"})
 
-        // add slots to array
-        timeSlots.push ({
+        let day = currentDate.getDate()
+        let month = currentDate.getMonth() + 1
+        let year = currentDate.getFullYear()
+
+        const slotDate = day+"_"+month+"_"+year
+        const slotTime = formattedTime
+
+        const isSlotAvailable = docInfo.slots_booked[slotDate] && docInfo.slots_booked[slotDate].includes(slotTime) ? false : true
+
+        if(isSlotAvailable){
+          // add slots to array
+          timeSlots.push ({
           datetime: new Date(currentDate),
           time: formattedTime
         })
+        }
+        
 
         // Increment current time by 30 minutes
         currentDate.setMinutes(currentDate.getMinutes() + 30)
